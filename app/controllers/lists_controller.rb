1299511da3ec01623cc63c576d7d09a69ee3d15b
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-09-24 15:28:57 +0000

    [finish]Rilsを学ぼう10章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -32,6 +35,8 @@ class ListsController < ApplicationController
     list.destroy
     redirect_to '/lists'
   end
+  
+  
 
   private
   def list_params
